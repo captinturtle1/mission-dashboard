9834260428b532fbeae39d2cc1dec0859ea4cb10
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2025-10-04 20:52:50 -0600

    updated image api endpoint

--- src/components/MissionCard.tsx
+++ src/components/MissionCard.tsx
@@ -2,7 +2,7 @@ export default function MissionCard({ name, image }: { name: string; image: stri
     return (
         <div className='border p-4 bg-gray-500 hover:bg-gray-600 rounded transition-colors cursor-pointer'>
             <h2 className='text-xl font-bold'>{name}</h2>
-            <img src={`http://localhost:8080/images/${image}`} alt={name} />
+            <img src={`http://localhost:8080/image/${image}`} alt={name} />
         </div>
     )
 }
